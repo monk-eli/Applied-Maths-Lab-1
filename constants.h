aded21570882fc53dbb0c5ad9cdcc5b5b9d3e466
monk-eli <zafersmilo@gmail.com>
2022-03-25 17:15:11 +0300

    feat: create .gitignore

--- constants.h
+++ constants.h
@@ -4,12 +4,12 @@
 #include <string>
 #include <cmath>
 
-const double EPS = 0.1;
+const double EPS = 0.00001;
 const double D = 0.00000005; // Delta for Dichotomy method
 const double A0 = 2.5;
 const double B0 = 7.5;
 const double GR = (1 + std::sqrt(5)) / 2; // Golden Ratio
 
-const std::string PATH {"/home/eliah/1.LABS/Applied Maths/Applied-Maths-Lab-1/lab1out.txt"};
+const std::string PATH {"/home/eliah/1.LABS/Applied Maths/Applied-Maths-Lab-1/lab1out.txt"}; // Insert your path to lab1out.txt
 
 #endif //LAB_1_CONSTANTS_H
